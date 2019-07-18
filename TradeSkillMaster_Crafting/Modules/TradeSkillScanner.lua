30bf34c0bfdafbd392ce47ff66b7d3a162ff2fad
Joshua Stachyshyn <Jashooa@gmail.com>
2019-07-18 16:59:29 +1200

    change tradeskillscanner

--- TradeSkillMaster_Crafting/Modules/TradeSkillScanner.lua
+++ TradeSkillMaster_Crafting/Modules/TradeSkillScanner.lua
@@ -262,13 +262,67 @@ end
 
 
 function private.GetProfessions()
-	local primary1, primary2, _, _, secondary1, secondary2 = GetProfessions()
-	if not (primary1 or primary2 or secondary1 or secondary2) then return end
-	return { primary1, primary2, secondary1, secondary2 }
+    local primary = {}
+    local prof1
+    local prof2
+    local cooking
+    local firstAid
+
+    for i = 1, GetNumSkillLines() do
+        if GetSkillLineInfo(i) == "Professions" then
+            i = i + 1
+            while not select(2, GetSkillLineInfo(i)) do
+                table.insert(primary, i)
+                i = i + 1
+            end
+        elseif GetSkillLineInfo(i) == "Secondary Skills" then
+            i = i + 1
+            while not select(2, GetSkillLineInfo(i)) do
+                local name = GetSkillLineInfo(i)
+                if name == "Cooking" then
+                    cooking = i
+                elseif name == "First Aid" then
+                    firstAid = i
+                end
+                i = i + 1
+            end
+        end
+    end
+
+    prof1 = primary[1]
+    prof2 = primary[2]
+
+    return { prof1, prof2, cooking, firstAid }
+end
+
+
+-- Helper function to find spellID associated to spellname
+local function GetTradeSkillSpellID(spellName)
+    -- GetTradeSkillRecipeLink ONLY works when a trade skill window is open, but this should always happen
+    if spellName == "Smelting" then
+        spellName = "Mining"
+    end
+	for i = 1,GetNumTradeSkills() do
+		local link = GetTradeSkillRecipeLink(i)
+		if link and link:match(spellName) then -- Not a header and spell name found
+			local spellID = tonumber(link:match("enchant:(%d+)"))
+			if spellID then
+				-- print(spellID, spellName, link)
+				return spellID
+			end
+		end
+    end
+    TSM:Printf("Could not find spellID for %s", spellName)
+	return nil
 end
 
 
 function private.UpdatePlayerTradeSkillsThread(self)
+	if (select(2, GetTradeSkillInfo(1)) ~= "header") then
+		TSM:Print("Tradeskill is not fully loaded, try to scan it later.")
+		return
+    end
+
 	self:SetThreadName("CRAFTING_PLAYER_TRADESKILLS")
 	-- get the player name
 	local playerName = self:WaitForFunction(UnitName, "player")
@@ -276,12 +330,10 @@ function private.UpdatePlayerTradeSkillsThread(self)
 	-- get the player's tradeskills
 	local oldTradeSkills = TSM.db.factionrealm.playerProfessions[playerName] or {}
 	local newTradeSkills = {}
-	SpellBook_UpdateProfTab()
 	local tradeSkills = self:WaitForFunction(private.GetProfessions)
-	local btns = { PrimaryProfession1SpellButtonBottom, PrimaryProfession2SpellButtonBottom, SecondaryProfession3SpellButtonRight, SecondaryProfession4SpellButtonRight }
-	for i, id in pairs(tradeSkills) do -- needs to be pairs since there might be holes
-		if not btns[i]:GetParent().missingHeader:IsVisible() then
-			local skillName, _, level, maxLevel = self:WaitForFunction(GetProfessionInfo, id)
+    for i, id in pairs(tradeSkills) do -- needs to be pairs since there might be holes
+        local skillName, _, _, level, _, _, maxLevel = GetSkillLineInfo(id)
+		if skillName then
 			newTradeSkills[skillName] = {}
 			newTradeSkills[skillName].level = level
 			newTradeSkills[skillName].maxLevel = maxLevel
